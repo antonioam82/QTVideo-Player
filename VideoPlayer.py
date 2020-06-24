3bba3e309222525a7f2b918de3571fd979ae2584
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-24 16:31:18 +0200

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -8,24 +8,24 @@ from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QMainWindow,
                              QVBoxLayout)
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-
-VIDEO_PATH = "" #"wild pogo mv.avi"#"city night lights.avi"#"video.avi" # vnideo.avi"
+# Ruta del archivo.
+VIDEO_PATH = ""#"wild pogo mv.avi"#"city night lights.avi"#"video.avi" # vnideo.avi"
 class MainWindow(QMainWindow):
     
     def __init__(self):
         super().__init__()
-        
-        # Controles principales para organizar la ventana.
+
         self.widget = QWidget(self)
         self.layout = QVBoxLayout()
         self.bottom_layout = QHBoxLayout()
-        
+
         self.video_widget = QVideoWidget(self)
         self.media_player = QMediaPlayer()
         self.media_player.setMedia(
             QMediaContent(QUrl.fromLocalFile(VIDEO_PATH)))
         self.media_player.setVideoOutput(self.video_widget)
-        
+
+        self.search_button = QPushButton("Buscar",self)
         self.play_button = QPushButton("Pausa", self)
         self.stop_button = QPushButton("Detener", self)
 
@@ -41,25 +41,25 @@ class MainWindow(QMainWindow):
 
         self.layout.addWidget(self.video_widget)
         self.layout.addLayout(self.bottom_layout)
+        self.bottom_layout.addWidget(self.search_button)
         self.bottom_layout.addWidget(self.play_button)
         self.bottom_layout.addWidget(self.stop_button)
         self.bottom_layout.addWidget(self.volume_slider)
         self.layout.addWidget(self.seek_slider)
-        
-        # Conectar los eventos con sus correspondientes funciones.
+
         self.play_button.clicked.connect(self.play_clicked)
         self.stop_button.clicked.connect(self.stop_clicked)
         self.media_player.stateChanged.connect(self.state_changed)
         
         self.video_widget.installEventFilter(self)
-        
+
         self.setWindowTitle("Reproductor de video")
         self.resize(800, 600)
         self.layout.setContentsMargins(0, 0, 0, 0)
         self.bottom_layout.setContentsMargins(0, 0, 0, 0)
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
-        
+
         self.media_player.play()
     
     def play_clicked(self):
