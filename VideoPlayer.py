292ea938a6109013c40478058a063adbb5ced504
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-27 14:07:22 +0200

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -97,7 +97,8 @@ class MainWindow(QMainWindow):
         fileName,_ = QFileDialog.getOpenFileName(self, "Archivo de video", '/home')#QDir.homePath())
         if fileName != '':
             videoName = fileName.split("/")[-1]
-            self.title_label.setText('VIDEO: {}'.format(videoName))
+            #self.title_label.setText('VIDEO: {}'.format(videoName))
+            self.title_label.setText(videoName)
             VIDEO_PATH = fileName
             self.media_player.setMedia(
             QMediaContent(QUrl.fromLocalFile(VIDEO_PATH)))
